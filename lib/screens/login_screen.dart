4ae2461ad33842a86decfbacd4210da213b777ad
alvine2200 <alvinellavu@gmail.com>
2023-05-30 17:07:50 +0300

    logowidget

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -14,6 +14,8 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
@@ -21,9 +23,32 @@ class _LoginScreenState extends State<LoginScreen> {
               hexStringToColor("9546C4"),
               hexStringToColor("5E61F4")
             ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/BigPen.jpeg"),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Image logoWidget(String imageName) {
+    return Image.asset(
+      imageName,
+      fit: BoxFit.cover,
+      width: 240,
+      height: 240,
+      // color: Colors.white,
     );
   }
 }
