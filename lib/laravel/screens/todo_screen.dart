91b2df4fae31f88223908ee36fba6c3aa4cc6eec
alvine2200 <alvinellavu@gmail.com>
2023-06-06 10:19:57 +0300

    git textfield and todo

--- lib/laravel/screens/todo_screen.dart
+++ lib/laravel/screens/todo_screen.dart
@@ -14,9 +14,39 @@ class _TodoAppState extends State<TodoApp> {
       appBar: AppBar(
         title: const Text('Todo App'),
         backgroundColor: Colors.blueGrey,
+        centerTitle: true,
       ),
       body: Container(
         margin: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey.shade900,
+                          ),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        hintText: 'Enter A todo'),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    //
+                  },
+                  child: const Text('Save'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
